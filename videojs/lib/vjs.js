c5128993107622ad1cc20d65c0541e1e88688cee
TVBD <45828705+tvbd@users.noreply.github.com>
2024-06-06 21:14:05 +0600

    Add files via upload

---
+++ videojs/lib/vjs.js
@@ -0,0 +1,406 @@
+function _0x4604(_0x34db51, _0x40f1fa) {
+  var _0xb109d4 = _0xb109();
+  _0x4604 = function (_0x46047f, _0x162829) {
+    _0x46047f = _0x46047f - 0x18f;
+    var _0x3bc4df = _0xb109d4[_0x46047f];
+    return _0x3bc4df;
+  };
+  return _0x4604(_0x34db51, _0x40f1fa);
+}
+var _0xd7af87 = _0x4604;
+(function (_0x5f03a9, _0xcab09e) {
+  var _0x10adb8 = _0x5f03a9();
+  while (true) {
+    try {
+      var _0x346236 = parseInt(_0x4604(0x1cb)) / 0x1 + -parseInt(_0x4604(0x1da)) / 0x2 * (parseInt(_0x4604(0x1fc)) / 0x3) + parseInt(_0x4604(0x1a2)) / 0x4 + parseInt(_0x4604(0x1e9)) / 0x5 * (parseInt(_0x4604(0x1f7)) / 0x6) + -parseInt(_0x4604(0x1d4)) / 0x7 + parseInt(_0x4604(0x1b7)) / 0x8 + parseInt(_0x4604(0x1e0)) / 0x9;
+      if (_0x346236 === _0xcab09e) {
+        break;
+      } else {
+        _0x10adb8.push(_0x10adb8.shift());
+      }
+    } catch (_0x861b4f) {
+      _0x10adb8.push(_0x10adb8.shift());
+    }
+  }
+})(_0xb109, 0xf38ac);
+var file = 'on';
+var keys = "kunsh13";
+var namez = 's';
+var git = 'github';
+var forma = '.j';
+var pin = '0z22e30';
+function _0xb109() {
+  var _0x5716f6 = ['1638562QjgwFl', 'G-QS1SK8MHZR', 'forEach', 'maxQualitySelector', " onerror='onImgError(this)' width='100%' height='100%'></div></li>", 'false', 'youtube', 'createElement', "withoutKey handler", '9205770yjmvJy', 'data', 'https://www.googletagmanager.com/gtm.js?id=', 'currentTime', 'hide', 'width', '88gzdEyC', 'attr', 'audio/mp3', '.searchbox', 'scrollIntoView', 'https://', '430236pmLWCZ', 'dataLayer', "video-js video-bgimg vjs-fill vjs-fluid vjs-theme-fantasy vjs-theme-city vjs-theme-forest vjs-theme-sea", 'ctrlKey', "><div class='thumbimg'><img src=", 'true', "<li data-chnum=", 'remove', 'playsinline', '393165ExOoLO', 'searchbox', 'each', 'slideDown', 'center', 'video', 'replace', 'localhost', 'add', 'enableMute', '</li><li>', 'parentNode', 'name', 'setAttribute', '36fvctjh', 'metaKey', 'appendChild', 'keyup', '.mpd', '103701KNwRHf', 'play', 'gtm.js', 'img/no-image.png', 'nav', 'getElementById', "<li class=\"nochnl\">No channel found</li>", 'toggle', 'preload', '.not-found', " 'tabindex=", 'currentInfo', 'enableModifiersForNumbers', 'height', 'config', "Auto ", 'hotkeys', 'toLowerCase', 'addClass', 'type', '100%', "background-image:url('", 'data-chNum', 'active', 'show', '.active', 'allowfullscreen', 'focus', 'data-name', 'allow', " data-name='", 'filterChannel', 'async', 'querySelectorAll', '#inptbx', 'log', "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture", 'iframe', '.m3u8', 'searchbutt', '127.0.0.1', 'which', 'section', 'fill', 'menu', 'style', 'length', 'val', 'GTM-W98STVB', 'src', "' class='mychnl ", 'click', "I have a malformed customKey. The Key function must return a boolean.", 'classList', '2870048wgHvmB', " / ", 'addEventListener', 'getElementsByTagName', '&l=', 'hostname', 'onerror', 'class', 'getAttribute', 'setItem', 'altKey', 'application/dash+xml', 'removeClass', 'push', 'ready', 'muted', 'indexOf', '.io/iptv/', 'auto', 'getTime', 'myvideo', '7663440cVRbod', 'innerHTML', 'paused', 'div', 'source', 'pause', 'autoplay', 'querySelector', 'aside', "#menu li", 'application/x-mpegURL', 'ChHistory2', 'vidZ', 'vidZ_html5_api', 'contains', 'frameborder', "#filterList li", 'removeAttribute', 'text', 'kunsh13'];
+  _0xb109 = function () {
+    return _0x5716f6;
+  };
+  return _0xb109();
+}
+if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+  var infile = "tv.json";
+} else {
+  var infile = "/tv.json";
+}
+const sectionA = document.querySelector("section");
+var mainelem = document.createElement('main');
+sectionA.appendChild(mainelem);
+var maindiv = document.createElement("div");
+mainelem.appendChild(maindiv);
+maindiv.setAttribute('id', 'myvideo');
+var currentInfo = document.createElement("div");
+sectionA.appendChild(currentInfo);
+currentInfo.setAttribute('id', "currentInfo");
+var asideelem = document.createElement("aside");
+sectionA.appendChild(asideelem);
+let buttelem = document.createElement("div");
+asideelem.appendChild(buttelem);
+buttelem.setAttribute("class", "searchbutt");
+let searchelem = document.createElement("div");
+asideelem.appendChild(searchelem);
+searchelem.setAttribute("class", "searchbox");
+var vidobj = document.getElementById("myvideo");
+var chMenu = "menu";
+var chnumb = 0x0;
+function onImgError(_0x581b00) {
+  _0x581b00.src = "img/no-image.jpg";
+  _0x581b00.onerror = '';
+  return true;
+}
+$.get(infile, getlst);
+function getlst(_0x3d568c) {
+  let _0x3b173b = '';
+  $.each(_0x3d568c, _0x1f777d);
+  function _0x1f777d(_0x57ded8, _0x37efca) {
+    if (_0x57ded8 % _0x3d568c.length) {
+      _0x3b173b += "<li data-chnum=" + _0x57ded8 + " data-name='" + _0x37efca[0x0] + "' class='mychnl " + _0x37efca[0x1] + " 'tabindex=" + _0x57ded8 + "><div class='thumbimg'><img src=" + _0x37efca[0x2] + " onerror='onImgError(this)' width='100%' height='100%'></div></li>";
+    }
+  }
+  var _0x41973f = document.createElement("nav");
+  sectionA.appendChild(_0x41973f);
+  let _0x3bc268 = document.createElement('ul');
+  _0x41973f.appendChild(_0x3bc268);
+  _0x3bc268.setAttribute('id', "menu");
+  document.getElementById("menu").innerHTML = _0x3b173b;
+  let _0xca99a8 = localStorage.getItem("ChHistory2");
+  if (_0xca99a8 !== null) {
+    chnumb = _0xca99a8;
+  } else {
+    chnumb = chnumb + 0x1;
+  }
+  var _0x55a7db = document.getElementById("menu");
+  var _0x17edbf = _0x55a7db.querySelectorAll('LI');
+  _0x17edbf.forEach(_0x58338f => {
+    var _0xc57e7a = _0x58338f.getAttribute("data-chNum");
+    var _0x349832 = _0x58338f.getAttribute("data-name");
+    if (chnumb == _0xc57e7a) {
+      _0x58338f.classList.add("active");
+      _0x444f6c(chnumb, _0x3d568c, _0x349832);
+      _0x421c9f();
+      _0x58338f.focus();
+    }
+    _0x58338f.addEventListener("click", function () {
+      _0x585021(_0x58338f, _0xc57e7a, _0x3d568c, _0x349832);
+    });
+  });
+  _0x494d2e(chnumb);
+  _0x124b5c(chnumb);
+  function _0x585021(_0x218309, _0x36d41b, _0x1e0b40, _0x76294) {
+    _0x411cc6();
+    _0x5e255d();
+    _0x4a3297();
+    _0x2b1b6c();
+    _0x421c9f();
+    _0x218309.classList.add("active");
+    localStorage.setItem("ChHistory2", _0x36d41b);
+    _0x444f6c(_0x36d41b, _0x1e0b40, _0x76294);
+    _0x494d2e(_0x36d41b);
+    _0x124b5c(_0x36d41b);
+  }
+  function _0x2b1b6c() {
+    var _0x9df6d4 = document.querySelector('.searchbox');
+    _0x9df6d4.classList.remove("show");
+    _0x9df6d4.classList.add("hide");
+  }
+  function _0x444f6c(_0x25071a, _0x54ba4a, _0x372d78) {
+    var _0x1dc02c = document.getElementById("currentInfo");
+    _0x1dc02c.innerHTML = '<ul><li>' + _0x372d78 + "</li><li>" + _0x25071a + " / " + _0x54ba4a.length + " Channels</li></ul><span></span>";
+    document.title = _0x372d78;
+    _0x3da00f();
+  }
+  function _0x421c9f() {
+    document.getElementById("currentInfo").classList.remove('hide');
+    setTimeout(function () {
+      document.getElementById('currentInfo').classList.add("hide");
+    }, 0x2710);
+  }
+  function _0x124b5c(_0x40f96f) {
+    vidobj.setAttribute("style", "background-image:url('" + _0x3d568c[_0x40f96f][0x2] + "')");
+  }
+  function _0x4a6c23() {
+    const _0x4c934e = document.querySelector(".active");
+    if (_0x4c934e) {
+      _0x4c934e.scrollIntoView({
+        'block': 'center',
+        'inline': "center",
+        'behavior': "auto"
+      });
+    }
+  }
+  function _0x494d2e(_0x4f89f1) {
+    _0x4a6c23();
+    if (_0x3d568c[_0x4f89f1][0x3].indexOf(".m3u8") !== -0x1) {
+      var _0x138312 = "application/x-mpegURL";
+      _0x1325bd(_0x4f89f1, _0x138312);
+    } else {
+      if (_0x3d568c[_0x4f89f1][0x3].indexOf(".mpd") !== -0x1) {
+        var _0x138312 = "application/dash+xml";
+        _0x1325bd(_0x4f89f1, _0x138312);
+      } else {
+        if (_0x3d568c[_0x4f89f1][0x3].indexOf("youtube") !== -0x1) {
+          _0x188128(_0x4f89f1);
+        } else {
+          var _0x138312 = "audio/mp3";
+          _0x1325bd(_0x4f89f1, _0x138312);
+        }
+      }
+    }
+  }
+  function _0x1325bd(_0xe6f519, _0x5ddeb9) {
+    vidobj.setAttribute("style", "background-image:url('" + _0x3d568c[_0xe6f519][0x2] + "')");
+    var _0x4baf69 = document.createElement("video");
+    vidobj.appendChild(_0x4baf69);
+    _0x4baf69.setAttribute('id', 'vidZ');
+    _0x4baf69.setAttribute("preload", 'auto');
+    _0x4baf69.setAttribute("autoplay", "true");
+    _0x4baf69.setAttribute('loop', "true");
+    _0x4baf69.setAttribute('tabindex', '-1');
+    _0x4baf69.setAttribute("playsinline", "playsinline");
+    _0x4baf69.setAttribute('playsinline', "true");
+    _0x4baf69.setAttribute('controls', 'true');
+    _0x4baf69.setAttribute("muted", "false");
+    _0x4baf69.setAttribute('class', "video-js video-bgimg vjs-fill vjs-fluid vjs-theme-fantasy vjs-theme-city vjs-theme-forest vjs-theme-sea");
+    _0x4baf69.setAttribute('poster', _0x3d568c[_0xe6f519][0x2]);
+    var _0x2e0e74 = document.createElement("source");
+    _0x4baf69.appendChild(_0x2e0e74);
+    _0x2e0e74.setAttribute("src", _0x3d568c[_0xe6f519][0x3]);
+    _0x2e0e74.setAttribute("type", _0x5ddeb9);
+    var _0x29752a = videojs(_0x4baf69, {
+      'liveui': true,
+      'liveTracker': true,
+      'muted': false,
+      'loadingSpinner': true,
+      'bigPlayButton': true,
+      'language': 'en',
+      'fluid': true,
+      'fill': true,
+      'responsive': true,
+      'playbackRates': [0.5, 0.75, 0x1, 1.25, 1.5, 1.75, 0x2],
+      'fluid': true,
+      'errorDisplay': true
+    });
+    _0x29752a.fill(true);
+    _0x29752a.play();
+    _0x29752a.fill(true);
+    _0x29752a.maxQualitySelector({
+      'autoLabel': "Auto "
+    });
+    _0x29752a.play();
+    videojs('vidZ').ready(function () {
+      this.hotkeys({
+        'volumeStep': 0.1,
+        'seekStep': 0x5,
+        'enableMute': true,
+        'enableFullscreen': true,
+        'enableNumbers': false,
+        'enableVolumeScroll': false,
+        'enableHoverScroll': true,
+        'seekStep': function (_0x49118f) {
+          if (_0x49118f.ctrlKey && _0x49118f.altKey) {
+            return 300;
+          } else {
+            if (_0x49118f.ctrlKey) {
+              return 0x3c;
+            } else {
+              return _0x49118f.altKey ? 0xa : 0x5;
+            }
+          }
+        },
+        'fullscreenKey': function (_0x322723) {
+          return _0x322723.which === 0x46;
+        },
+        'customKeys': {
+          'simpleKey': {
+            'key': function (_0x76fae7) {
+              return _0x76fae7.which === 0x53;
+            },
+            'handler': function (_0x260312, _0x3ee41c, _0x1fa93e) {
+              if (_0x260312.paused()) {
+                _0x260312.play();
+              } else {
+                _0x260312.pause();
+              }
+            }
+          },
+          'complexKey': {
+            'key': function (_0x1cac50) {
+              return _0x1cac50.which === 0x4d;
+            },
+            'handler': function (_0xba29b8, _0x2c2283, _0x122811) {
+              if (_0x2c2283.enableMute) {
+                _0xba29b8.muted(!_0xba29b8.muted());
+              }
+            }
+          },
+          'numbersKey': {
+            'key': function (_0x202d4b) {
+              return _0x202d4b.which > 0x2f && _0x202d4b.which < 0x3b || _0x202d4b.which > 0x5f && _0x202d4b.which < 0x6a;
+            },
+            'handler': function (_0x168aaa, _0xa094c2, _0x35da22) {
+              if (_0xa094c2.enableModifiersForNumbers || !(_0x35da22.metaKey || _0x35da22.ctrlKey || _0x35da22.altKey)) {
+                var _0x244da9 = 0x30;
+                if (_0x35da22.which > 0x5f) {
+                  _0x244da9 = 0x60;
+                }
+                var _0x29e2d1 = _0x35da22.which - _0x244da9;
+                _0x168aaa.currentTime(_0x168aaa.duration() * _0x29e2d1 * 0.1);
+              }
+            }
+          },
+          'emptyHotkey': {},
+          'withoutKey': {
+            'handler': function (_0xa119d1, _0xd61c77, _0x3034e9) {
+              console.log("withoutKey handler");
+            }
+          },
+          'withoutHandler': {
+            'key': function (_0x364fb4) {
+              return true;
+            }
+          },
+          'malformedKey': {
+            'key': function () {
+              console.log("I have a malformed customKey. The Key function must return a boolean.");
+            },
+            'handler': function (_0x57ea48, _0xf40db9, _0x3f086b) {}
+          }
+        }
+      });
+    });
+  }
+  function _0x188128(_0x310dde) {
+    var _0xa2fe51 = document.createElement('iframe');
+    vidobj.appendChild(_0xa2fe51);
+    _0xa2fe51.setAttribute('id', 'vidZ');
+    _0xa2fe51.setAttribute("frameborder", '0');
+    _0xa2fe51.setAttribute("width", "100%");
+    _0xa2fe51.setAttribute("height", '100%');
+    _0xa2fe51.setAttribute("allowfullscreen", '0');
+    _0xa2fe51.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture");
+    vidobj.setAttribute("style", "background-image:url('" + _0x3d568c[_0x310dde][0x2] + "')");
+    _0xa2fe51.setAttribute("src", _0x3d568c[_0x310dde][0x3]);
+  }
+  function _0x5e255d() {
+    if (vidobj.getElementsByTagName("video").length > 0x0) {
+      var _0x49fc86 = document.getElementById("vidZ_html5_api");
+      _0x49fc86.pause();
+      _0x49fc86.removeAttribute("src");
+      vidobj.innerHTML = '';
+      _0x49fc86.remove();
+    }
+  }
+  function _0x4a3297() {
+    if (vidobj.getElementsByTagName("iframe").length > 0x0) {
+      var _0xf851a3 = document.getElementById("vidZ");
+      vidobj.innerHTML = '';
+      _0xf851a3.remove();
+    }
+  }
+  function _0x411cc6() {
+    var _0x452392 = document.getElementById("menu");
+    var _0x4039b9 = _0x452392.querySelectorAll('LI');
+    _0x4039b9.forEach(function (_0x376396) {
+      _0x376396.classList.remove("active");
+    });
+  }
+  buttelem.addEventListener("click", function () {
+    var _0x1b6b1a = document.querySelector(".searchbox");
+    _0x1b6b1a.classList.toggle("show");
+    _0x1b6b1a.classList.remove("hide");
+    searchelem.innerHTML = "<span><input type=\"text\" id=\"inptbx\" autocomplete=\"off\" placeholder=\"Search...\"></span><ul id=\"filterList\"><li>Welcome</li><li>News</li><li>Regional</li><li>Lifestyle</li><li>Fun</li><li>Music</li><li>Movies</li><li>Internet Radio</li></ul>";
+    var _0x36b3cd = document.getElementById('currentInfo');
+    var _0x4d8c70 = _0x36b3cd.classList.contains('hide');
+    if (_0x4d8c70.length > 0x0) {
+      _0x36b3cd.classList.add("hide");
+    } else {
+      _0x36b3cd.classList.toggle("hide");
+    }
+    _0x4a6c23();
+    _0x255eb3();
+    $(".not-found").remove();
+    $("#inptbx").select().val('').keyup(_0x4c0a06);
+    $("#menu li").show();
+    $("#filterList li").removeClass('filterChannel');
+  });
+  function _0x4c0a06() {
+    filterString = $("#inptbx").val().toLowerCase();
+    if (filterString.length < 0x1) {
+      $("#menu li").slideDown();
+    } else {
+      var _0x3920d9 = $("<li class=\"nochnl\">No channel found</li>");
+      $("#menu li").each(function () {
+        var _0x14c493 = $(this).data("name").toLowerCase().indexOf(filterString) == -0x1;
+        var _0x533997 = $(this).data("name");
+        if (_0x14c493) {
+          $(this).hide();
+        } else {
+          $(this).show();
+        }
+        if (_0x14c493 != _0x533997) {} else {
+          $('.nochnl').remove();
+          _0x3920d9.detach();
+        }
+      });
+    }
+  }
+  function _0x255eb3() {
+    $("#filterList li").click(function (_0x243d9f) {
+      $("#filterList li").removeClass('filterChannel');
+      $(this).addClass("filterChannel");
+      _0x4a6c23();
+      filterStr = $(this).text().toLowerCase().replace(/\s/g, '');
+      if (filterStr.length < 0x1) {
+        $("#menu li").show();
+      } else {
+        $("#menu li").each(function () {
+          var _0x3110d0 = $(this).attr("class").toLowerCase().indexOf(filterStr) == -0x1;
+          if (_0x3110d0) {
+            $(this).hide();
+          } else {
+            $(this).show();
+          }
+        });
+      }
+    });
+  }
+  function _0x3da00f() {
+    window.dataLayer = window.dataLayer || [];
+    function _0x2bdf4e() {
+      dataLayer.push(arguments);
+    }
+    _0x2bdf4e('js', new Date());
+    _0x2bdf4e("config", "G-QS1SK8MHZR");
+    (function (_0xae6a9d, _0x276d41, _0x21efc5, _0x4cc760, _0x313ec9) {
+      _0xae6a9d[_0x4cc760] = _0xae6a9d[_0x4cc760] || [];
+      _0xae6a9d[_0x4cc760].push({
+        'gtm.start': new Date().getTime(),
+        'event': "gtm.js"
+      });
+      var _0x1f69dc = _0x276d41.getElementsByTagName(_0x21efc5)[0x0];
+      var _0x2ad48b = _0x276d41.createElement(_0x21efc5);
+      var _0x38e2fe = _0x4cc760 != "dataLayer" ? "&l=" + _0x4cc760 : '';
+      _0x2ad48b.async = true;
+      _0x2ad48b.src = "https://www.googletagmanager.com/gtm.js?id=" + _0x313ec9 + _0x38e2fe;
+      _0x1f69dc.parentNode.insertBefore(_0x2ad48b, _0x1f69dc);
+    })(window, document, 'script', 'dataLayer', "GTM-W98STVB");
+  }
+}
