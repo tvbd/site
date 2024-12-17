235d46c995f6515ee5bceab1b44198025e02a2e6
TVBD <45828705+tvbd@users.noreply.github.com>
2024-12-17 13:35:45 +0600

    Create m3u.js

---
+++ orbify/m3u.js
@@ -0,0 +1,19 @@
+let currentServer = 'https://raw.githubusercontent.com/johirryhan087/iptv_playlists/refs/heads/main/https_playlist.m3u';
+
+const servers = [
+  'https://raw.githubusercontent.com/manikiptv/freecatv.github.io/refs/heads/main/freecatv.m3u8',
+    'https://raw.githubusercontent.com/johirryhan087/iptv_playlists/refs/heads/main/https_playlist.m3u',
+    'https://m3u.ch/pl/eb7353c3bab76a0cb6f1cb26de322332_fee60ab67cca804d148f4ef3a25487a7.m3u',
+    'https://m3u.ch/pl/a52c2078d071bf675f72f127d0c6113c_456204d01661f9fcc506243abf05db83.m3u',
+    'https://raw.githubusercontent.com/johirryhan087/iptv_playlists/refs/heads/main/http_playlist.m3u',
+   'https//raw.githubusercontent.com/Jeshan-akand/Toffee-Channels-Link-Headers/main/toffee_OTT_Navigator.m3u',
+  'https://your-abbu.pages.dev/SonyLIV.m3u',
+  'https://s1.sadman0.workers.dev/neonflix.m3u8',
+  'https://raw.githubusercontent.com/Miraz6755/Bdixtv/refs/heads/main/Miraz-tv-pro.m3u8',
+  'https://raw.githubusercontent.com/subirkumarpaul/Subirmaxpro/refs/heads/main/Subirmaxpro',
+  'https://raw.githubusercontent.com/Miraz6755/Iptv.m3u/refs/heads/main/Miraz%20tv',
+  'https://raw.githubusercontent.com/BDSTVHUB/BDStreamHub/refs/heads/main/index.html',
+  'https://raw.githubusercontent.com/xyz84dotcom/-/refs/heads/main/Ghoziiptv.m3u',
+  'https://raw.githubusercontent.com/abid58b/FanCodePlaylist/refs/heads/main/fancode.m3u',
+  'https://shz.al/~rcnmediawebtv294'
+];
