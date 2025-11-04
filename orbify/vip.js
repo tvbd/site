3065ed952b140541b2fd2eeed692205d4ac1c93a
TVBD <45828705+tvbd@users.noreply.github.com>
2025-11-04 23:35:42 +0600

    Create vip.js

---
+++ orbify/vip.js
@@ -0,0 +1,5 @@
+const servers = [
+  { name: 'Bdixiptvbd', url: 'https://web.bdixiptvbd.com/live.m3u' },
+  { name: 'Bugsfreeweb', url: 'https://raw.githack.com/bugsfreeweb/LiveTVCollector/refs/heads/main/LiveTV/Bangladesh/LiveTV.m3u' },
+];
+  
