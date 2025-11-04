e0b14787e282bd585136d034ef0f9e95f7c178f0
TVBD <45828705+tvbd@users.noreply.github.com>
2025-11-05 01:29:18 +0600

    Update vip.js

--- orbify/vip.js
+++ orbify/vip.js
@@ -1,5 +1,7 @@
 const servers = [
-  { name: 'Bdixiptvbd', url: 'https://web.bdixiptvbd.com/live.m3u' },
-  { name: 'Bugsfreeweb', url: 'https://raw.githack.com/bugsfreeweb/LiveTVCollector/refs/heads/main/LiveTV/Bangladesh/LiveTV.m3u' },
+  { name: 'Bdixiptvbd', url: 'http://web.bdixiptvbd.com/live.m3u' },
+  { name: 'Bugsfreeweb', url: 'http://raw.githack.com/bugsfreeweb/LiveTVCollector/refs/heads/main/LiveTV/Bangladesh/LiveTV.m3u' },
+ { name: 'Freecatv', url: 'http://raw.githubusercontent.com/manikiptv/freecatv.github.io/refs/heads/main/freecatv.m3u8' },
+
 ];
   
